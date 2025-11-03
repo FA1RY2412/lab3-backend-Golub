8a5b7542d48d5b8d62e615372e19ee8fc825aaa0
FA1RY2412 <denisgolub2412@gmail.com>
2025-11-03 19:36:05 +0200

    Delete OneDrive/Рабочий стол/5 Семестр/БЕК/lab3 directory

--- OneDrive/Рабочий стол/5 Семестр/БЕК/lab3/app/config.py
+++
@@ -1,6 +0,0 @@
-
-# Default config file loaded by app.config.from_pyfile('config.py', silent=True)
-import os
-# If DATABASE_URL not provided, will fallback to sqlite in __init__.py
-SQLALCHEMY_ECHO = False
-JSON_SORT_KEYS = False
